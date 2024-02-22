1929d03e534301b5f5a8e370a170491911babb93
Jacek programmer <jacek301@gmail.com>
2024-02-22 22:40:05 +0100

    NextTest

--- src/test/java/org/kata/training/StackTest.java
+++ src/test/java/org/kata/training/StackTest.java
@@ -77,4 +77,14 @@ public class StackTest {
     public void defaultMaxElementInStackIs5() {
         assertEquals(5,stack.getMaxElements());
     }
+
+    @Test
+    public void byDefaultCanInsert5ElementsOnStack() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        assertEquals(5,stack.getMaxElements());
+    }
 }
